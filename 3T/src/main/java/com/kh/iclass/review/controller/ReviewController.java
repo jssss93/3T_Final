a49f8a51c92a4114173e07b05b68d51684b9fe03
DESKTOP-THHBRLB\호준 <ho-jun888@daum.net>
2018-08-13 17:01:53 +0900

    2018-08-13  17:01  컨트롤러 수정

--- 3T/src/main/java/com/kh/iclass/review/controller/ReviewController.java
+++ 3T/src/main/java/com/kh/iclass/review/controller/ReviewController.java
@@ -33,7 +33,7 @@ public class ReviewController {
 
 	@RequestMapping(value = "/review/writeForm")
 	public ModelAndView reviewWrite(CommandMap commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("/review/write");
+		ModelAndView mv = new ModelAndView("review/write");
 
 		return mv;
 	}
@@ -49,7 +49,7 @@ public class ReviewController {
 
 	@RequestMapping(value = "/review/detail")
 	public ModelAndView reviewDetail(CommandMap commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("/review/detail");
+		ModelAndView mv = new ModelAndView("review/detail");
 		System.out.println("reviewDetail : " + commandMap.getMap());
 		Map<String, Object> map = ReviewService.ReviewDetail(commandMap.getMap());
 
@@ -63,7 +63,7 @@ public class ReviewController {
 
 	@RequestMapping(value = "/review/updateForm")
 	public ModelAndView reviewUpdateForm(CommandMap commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("/review/update");
+		ModelAndView mv = new ModelAndView("review/update");
 
 		Map<String, Object> map = ReviewService.ReviewDetail(commandMap.getMap());
 		mv.addObject("map", map);
@@ -92,7 +92,7 @@ public class ReviewController {
 
 	@RequestMapping(value = "/review/comment")
 	public ModelAndView reviewCommentInsert(CommandMap commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("/review/detail");
+		ModelAndView mv = new ModelAndView("review/detail");
 
 		System.out.println("reviewCommentInsert : " + commandMap.getMap());
 		ReviewService.ReviewCommentWrite(commandMap.getMap());
@@ -111,7 +111,7 @@ public class ReviewController {
 
 	@RequestMapping(value = "/review/commentDelete")
 	public ModelAndView reviewCommentDelete(CommandMap commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("/review/detail");
+		ModelAndView mv = new ModelAndView("review/detail");
 		System.out.println("reviewCommentDelete : " + commandMap.getMap());
 		ReviewService.ReviewDeleteComment(commandMap.getMap());
 
